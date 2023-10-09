4aedf8c5b36c04038990daa688ee8b57598ff2d6
Maxim Keegan <keegan@tochka.com>
2023-10-09 21:38:21 +0500

    Initial Commit

---
+++ NextButtonBackground/NextButtonBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextButtonBackgroundApp.swift
+//  NextButtonBackground
+//
+//  Created by Maxim Keegan on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NextButtonBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
